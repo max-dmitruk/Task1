61145abfacd8cdde401d2c0ef0126d584adf2e39
f <f@DESKTOP-G1ULRF7>
2020-12-01 21:45:49 +0300

    Add project files.

---
+++ AirCompany/Classes/Airplane.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirCompany.Classes
+{
+    public abstract class Airplane
+    {
+        public string Name { get; set; }
+        public double EngineVolume { get; set; }
+        public int Capacity { get; set; }
+        public double Cost { get; set; }
+
+        public Airplane(string name, double engineVolume, int capacity, double cost) 
+        {
+            Name = name;
+            EngineVolume = engineVolume;
+            Capacity = capacity;
+            Cost = cost;
+        }
+        public abstract void Rise();
+        public abstract void Landing();
+    }
+}
---
+++ AirCompany/Classes/Cargo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirCompany.Classes
+{
+    public class Cargo : Airplane
+    {
+        
+    }
+}
